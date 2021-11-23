f93a034f6deead47ca09168ad7245d55b8754dfc
DanaMadar <dana.madar@gmail.com>
2021-11-23 14:14:43 +0200

    test

--- animals/urls.py
+++ animals/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path('', views.all_animal_vies, name='all_animals'),
+    path('<int:id>/', views.all_animal_vies, name='all_animals')
 ]
